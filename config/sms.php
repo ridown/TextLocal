7a0ed480993bf9fc1ba53e27757c47ad7bfc9dfa
Paul <paul@shopapps.co.uk>
2018-03-27 13:42:26 +0100

    Update sms.php

--- config/sms.php
+++ config/sms.php
@@ -1,19 +1,17 @@
 <?php
 
-	return array(
+return array(
 
-		/*
-		|--------------------------------------------------------------------------
-		| Settings
-		|--------------------------------------------------------------------------
-		*/
-		'default' => env('SMS_DRIVER', 'TextLocal'),
-    
-    'connections' => [
-      'TextLocal' => [
-		    'url'    => env('SMS_TEXTLOCAL_URL', 'http://api.txtlocal.com/send/'),
-        'key'    => env('SMS_TEXTLOCAL_KEY', ''),
-		    'sender' => env('SMS_TEXTLOCAL_NAME',''),
+	/*
+	 * Settings
+	 */
+	'default' => env('SMS_DRIVER', 'TextLocal'),
+    	'connections' => [
+      		'TextLocal' => [
+        		'key'    => env('SMS_TEXTLOCAL_KEY', ''),
+		    	'sender' => env('SMS_TEXTLOCAL_NAME',''),
+			'url'    => env('SMS_TEXTLOCAL_URL', 'http://api.txtlocal.com/send/'),
+			'format' => env('SMS_TEXTLOCAL_FORMAT, 'xml'),
 		  ]
-    ]
-	);
+    	]
+);
