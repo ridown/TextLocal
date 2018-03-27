8ceb711c897eed669ef55a70adea0015264da10b
Paul Robbins <paul@shopapps.co.uk>
2018-03-27 14:56:20 +0100

    no message

--- config/sms.php
+++ config/sms.php
@@ -11,7 +11,7 @@ return array(
         		'key'    => env('SMS_TEXTLOCAL_KEY', ''),
 		    	'sender' => env('SMS_TEXTLOCAL_NAME',''),
 			'url'    => env('SMS_TEXTLOCAL_URL', 'http://api.txtlocal.com/send/'),
-			'format' => env('SMS_TEXTLOCAL_FORMAT, 'xml'),
+			'format' => env('SMS_TEXTLOCAL_FORMAT', 'xml'),
 		  ]
     	]
 );
